c87ca69a25b1c083e767540d6d28db6b644ae436
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-11 10:02:01 -0300

    Aula 4 Integrando Junit

--- src/main/java/br/com/jonathankbp/servicos/LocacaoService.java
+++ src/main/java/br/com/jonathankbp/servicos/LocacaoService.java
@@ -6,6 +6,8 @@ import br.com.jonathankbp.entidades.Filme;
 import br.com.jonathankbp.entidades.Locacao;
 import br.com.jonathankbp.entidades.Usuario;
 import br.com.jonathankbp.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class LocacaoService {
 	
@@ -27,7 +29,8 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		//cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
@@ -37,9 +40,9 @@ public class LocacaoService {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 
 		//verificação
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(2)));
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
 	}
 }
