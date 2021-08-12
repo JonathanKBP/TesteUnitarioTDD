34941b6cf665c5975e7a1d7fc7cb7664fa0a92d9
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-12 10:19:08 -0300

    Aula 8 Formas de dividir um teste (ErrorCollector)

--- src/test/java/br/com/jonathankbp/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/jonathankbp/servicos/LocacaoServiceTest.java
@@ -4,7 +4,9 @@ import br.com.jonathankbp.entidades.Filme;
 import br.com.jonathankbp.entidades.Locacao;
 import br.com.jonathankbp.entidades.Usuario;
 import br.com.jonathankbp.utils.DataUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
@@ -14,8 +16,11 @@ import static org.junit.Assert.*;
 
 public class LocacaoServiceTest {
 
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
     @Test
-    public void teste() {
+    public void testeLocacao() {
         //cenario
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
@@ -25,9 +30,9 @@ public class LocacaoServiceTest {
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         //verificação
-        assertThat(locacao.getValor(), is(5.0));
-        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+        error.checkThat(locacao.getValor(), is(5.0));
+        error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 
     }
 }
