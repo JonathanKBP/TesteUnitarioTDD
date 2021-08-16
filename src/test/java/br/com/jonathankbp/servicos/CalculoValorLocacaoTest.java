fd06d1857312a74bb45a965880bef3a70a69b8aa
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-16 15:12:33 -0300

    Aula 18 Testes Parametrizaveis

---
+++ src/test/java/br/com/jonathankbp/servicos/CalculoValorLocacaoTest.java
@@ -0,0 +1,71 @@
+package br.com.jonathankbp.servicos;
+
+import br.com.jonathankbp.Exception.FilmeSemEstoqueException;
+import br.com.jonathankbp.Exception.LocadoraException;
+import br.com.jonathankbp.entidades.Filme;
+import br.com.jonathankbp.entidades.Locacao;
+import br.com.jonathankbp.entidades.Usuario;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.runners.Parameterized.*;
+
+@RunWith(Parameterized.class)
+public class CalculoValorLocacaoTest {
+
+    private LocacaoService service;
+
+    @Parameter
+    public List<Filme> filmes;
+
+    @Parameter(value = 1)
+    public Double valorLocacao;
+
+    @Parameter(value = 2)
+    public String cenario;
+
+    private static Filme filme1 = new Filme("Filme 1", 2, 4.0);
+    private static Filme filme2 = new Filme("filme 2", 2, 4.0);
+    private static Filme filme3 = new Filme("Filme 3", 2, 4.0);
+    private static Filme filme4 = new Filme("Filme 4", 2, 4.0);
+    private static Filme filme5 = new Filme("Filme 5", 2, 4.0);
+    private static Filme filme6 = new Filme("Filme 6", 2, 4.0);
+    private static Filme filme7 = new Filme("Filme 7", 2, 4.0);
+
+    @Parameters(name = "{2}")
+    public static Collection<Object[]> getParametros(){
+        return Arrays.asList(new Object[][]{
+                {Arrays.asList(filme1, filme2), 8.0, "2 Filmes: Sem desconto"},
+                {Arrays.asList(filme1, filme2, filme3), 11.0, "3 Filmes: 25%"},
+                {Arrays.asList(filme1, filme2, filme3, filme4), 13.0, "4 Filmes: 50%"},
+                {Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0, "5 Filmes: 75%"},
+                {Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 14.0, "6 Filmes: 100%"},
+                {Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7), 18.0, "7 Filmes: Sem Desconto"},
+        });
+    }
+
+    @Before
+    public void setup() {
+        service = new LocacaoService();
+    }
+
+    @Test
+    public void deveCalcularValorLocacaoConsiderandoDescontos() throws FilmeSemEstoqueException, LocadoraException {
+        //cenario
+        Usuario usuario = new Usuario("Usuario 1");
+
+        //acao
+        Locacao resultado = service.alugarFilme(usuario, filmes);
+
+        //verificacao
+        assertThat(resultado.getValor(), is(valorLocacao));
+    }
+}
