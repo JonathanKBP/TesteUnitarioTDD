8116ff67c55856ef8810c109844bbe309116fd0b
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-16 17:00:14 -0300

    Aula 21 Suite de testes

---
+++ src/test/java/br/com/jonathankbp/suites/SuitesExecucao.java
@@ -0,0 +1,17 @@
+package br.com.jonathankbp.suites;
+
+import br.com.jonathankbp.servicos.Calculadora;
+import br.com.jonathankbp.servicos.CalculadoraTest;
+import br.com.jonathankbp.servicos.CalculoValorLocacaoTest;
+import br.com.jonathankbp.servicos.LocacaoServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalculadoraTest.class,
+        CalculoValorLocacaoTest.class,
+        LocacaoServiceTest.class
+})
+public class SuitesExecucao {
+}
