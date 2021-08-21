fb786dff23dd73e3c0556b6181aa9eacf1c010d5
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-21 18:23:52 -0300

    Aula 44. Testes em Paralelo, parte 2

--- src/test/java/br/com/jonathankbp/servicos/CalculadoraTest.java
+++ src/test/java/br/com/jonathankbp/servicos/CalculadoraTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(ParallelRunner.class)
+
 public class CalculadoraTest {
 
     private Calculadora calc;
