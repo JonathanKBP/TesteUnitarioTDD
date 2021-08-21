73b2d7c1f67b9ca09f18b87a79b9b39a06615169
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-21 00:44:41 -0300

    Aula 34. Spy

--- src/test/java/br/com/jonathankbp/servicos/CalculadoraMockTest.java
+++ src/test/java/br/com/jonathankbp/servicos/CalculadoraMockTest.java
@@ -2,11 +2,37 @@ package br.com.jonathankbp.servicos;
 
 import br.com.jonathankbp.entidades.Locacao;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
+import org.mockito.*;
 
 public class CalculadoraMockTest {
+
+    @Mock
+    private Calculadora calcMock;
+
+    @Spy
+    private Calculadora calcSpy;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void devoMostrarDiferencaEntreMockSpy() {
+        Mockito.when(calcMock.somar(1, 2)).thenCallRealMethod();
+        Mockito.when(calcSpy.somar(1, 3)).thenReturn(8);
+        Mockito.doNothing().when(calcMock).imprime();
+
+        System.out.println("Mock:" + calcMock.somar(1,2));
+        System.out.println("Spy:" + calcSpy.somar(1,2));
+
+        System.out.println("Mock");
+        calcMock.imprime();
+        System.out.println("Spy");
+        calcSpy.imprime();
+    }
 
     @Test
     public void teste(){
