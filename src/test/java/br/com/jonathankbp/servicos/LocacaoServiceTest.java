a299cfbf5c7db34e6227bcb530f1d7d3098ea9bf
JonthanKBP <jonathan.kevinbp@hotmail.com>
2021-08-21 12:47:25 -0300

    Aula 39. Testando metodos privados diretamente

--- src/test/java/br/com/jonathankbp/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/jonathankbp/servicos/LocacaoServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.*;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -240,5 +241,17 @@ public class LocacaoServiceTest {
        //verificacao
        Assert.assertThat(locacao.getValor(), is(1.0));
        PowerMockito.verifyPrivate(service).invoke("CalcularValorLocacao", filmes);
+   }
+
+   @Test
+    public void deveCalcularValorLocacao() throws Exception {
+        //cenario
+       List<Filme> filmes = Arrays.asList(umFilme().agora());
+
+       //acao
+       Double valor = (Double) Whitebox.invokeMethod(service, "CalcularValorLocacao", filmes);
+
+       //verificao
+       Assert.assertThat(valor, is(4.0));
    }
 }
